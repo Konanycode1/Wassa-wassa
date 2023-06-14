a622d712d1071ac22f8f3393991400f8f7d08b15
kouame konan abraham <abrahamkonany@gmail.com>
2023-06-14 17:15:53 +0000

    ok

--- compagnie/assets/js/login.js
+++ compagnie/assets/js/login.js
@@ -1,3 +1,38 @@
 $(document).ready(()=>{
-    alert("ok")
+    let url = "https://wassa.onrender.com/api/compagnieLogin/"
+    let api = "http://localhost:3000/api/compagnieLogin/"
+    const closeButton = document.getElementById("closeDialog");
+    const dialog = document.getElementById("basicDialog");
+
+    $('#loginform').on("submit",(e)=>{
+        e.preventDefault();
+
+        let email = $("#email").val();
+        let password = $("#password").val();
+
+        let data = {
+            email: email,
+            password: password
+        }
+        fetch(api,{
+            method: "POST",
+            body: JSON.stringify(data)
+        })
+        .then((response)=>response.json())
+        .then((data)=>{
+            dialog.showModal();
+            $(".msg").text(data.msg).css('color', 'green')
+            closeButton.addEventListener("click", () => {
+                dialog.close(); 
+                // if(status == 200){
+                //     window.location.href = "../loginComp.html"
+                // }
+            });
+
+        })
+        .catch((err)=> console.log(err))
+
+
+
+    })
 })
--- compagnie/assets/js/signup.js
+++ compagnie/assets/js/signup.js
@@ -2,51 +2,58 @@ $(document).ready(()=>{
    
     let url = "https://wassa.onrender.com/api/compagnieCreate/"
     let api = "http://localhost:3000/api/compagnieCreate/"
+    let form = document.getElementById("signup")
+    let btn = document.getElementById("btnSign")
 
-    $(".signup").on('submit',(e)=>{
+const button = document.getElementById("buttonDialog");
+const closeButton = document.getElementById("closeDialog");
+
+const dialog = document.getElementById("basicDialog");
+
+    $("#signup").on("submit",(e)=>{
         e.preventDefault();
-    })
-
-    $(".btnSign").on("click",()=>{
-
+        let data = new FormData()
         let compagnie = $("#compagnie").val();
         let numero = $("#numero").val();
         let email = $("#email").val();
         let commune = $("#commune").val();
         let degret = $("#degret").val();
-        let logo = $("#logo").val();
+        let logo = $("#logo").get(0).files[0];
         let password = $("#password").val();
         let Cpassword = $("#Cpassword").val();
-        
 
+        data.append("compagnie", compagnie)
+        data.append("numero",numero)
+        data.append("email",email)
+        data.append("commune",commune)
+        data.append("degret",degret)
+        data.append("logo",logo)
+        data.append("password",password)
         if(password == Cpassword){
-            let data = {
-                compagnie:compagnie,
-                numero: numero,
-                email: email,
-                commune: commune,
-                degret: degret,
-                logo: logo,
-                password: password
-            }
             fetch(api, {
                 method: "POST",
-                body: JSON.stringify(data),
-                headers: {
-                    "Content-Type": "application/json"
-                }  
+                body: data, 
             })
-            .then((response)=> response.json())
+            .then((response)=> {
+                localStorage.setItem("status",response.status)
+                return response.json()   
+            })
             .then((data)=> {
+                const status = localStorage.getItem("status")
+                dialog.showModal();
                 $(".msg").text(data.msg).css('color', 'green')
+                closeButton.addEventListener("click", () => {
+                    dialog.close(); 
+                    if(status == 200){
+                        window.location.href = "../loginComp.html"
+                    }
+                }); 
             })
             .catch((err)=> console.log(err))
         }
         else{
             alert("mot de passe incorrecte")
         }
-
-        
 
     })
 })
